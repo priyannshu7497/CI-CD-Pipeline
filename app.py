2a18bffb8f3a97a0c2521591b5c40cd199bb2dc9
Priyanshu Gupt <162509941+priyannshu7497@users.noreply.github.com>
2025-12-05 16:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def hello():
     return "Hello from Flask CI/CD Pipeline!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# REMOVE debug and run() function because Gunicorn will handle it.
+
 
